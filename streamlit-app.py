3fa196471655f507f02040592e8b34a71fa1f421
Aaron Angeles <asa277@cornell.edu>
2024-12-24 23:09:54 -0500

    POC v2.0

--- streamlit-app.py
+++ streamlit-app.py
@@ -7,6 +7,7 @@ import json
 import requests
 from datetime import datetime
 import streamlit as st
+import plotly.express as px
 
 st.set_page_config(page_title="NFL Playcall Profiler", initial_sidebar_state="expanded")
 
@@ -32,14 +33,22 @@ with tab_dashboard:
     teams = load_teams()
 
     # Select Team -------------------------
-    selected_team = st.selectbox("NFL Team:", options=teams['display_name'])
-    selected_team_row = teams[teams['display_name'] == selected_team]
-    selected_team_image = selected_team_row.iloc[0]['default_logo']
-    st.image(selected_team_image, width = 150)
+    selected_team = st.multiselect("NFL Team:", options=teams['display_name'], default='Arizona Cardinals')
+    # Check if no team is selected
+    if not selected_team:
+        st.warning("Please select at least one NFL team!")
+    selected_team_rows = teams[teams['display_name'].isin(selected_team)]
+    selected_team_ids = selected_team_rows['teamId'].tolist()
+    if not selected_team_rows.empty:
+        for _, row in selected_team_rows.iterrows():
+            st.image(row['default_logo'], width=150, caption=row['display_name'])
 
 
     # Home / Away ---------------------------
-    st.multiselect("Setting: ", ['Home', 'Away'], default = ['Home', 'Away'])
+    homeaway = st.multiselect("Setting: ", ['Home', 'Away'], default = ['Home', 'Away'])
+    # Map selections to True/False values
+    homeaway_list = [True] if 'Home' in homeaway else []
+    homeaway_list += [False] if 'Away' in homeaway else []
 
     # Time Frame ----------------------------
     setting_list = {
@@ -50,7 +59,8 @@ with tab_dashboard:
         '2-Minute Drill': 5
         }
     setting_keys = setting_list.keys()
-    st.multiselect("Time: ", options=setting_keys, default = setting_keys)
+    timesetting = st.multiselect("Time: ", options=setting_keys, default = setting_keys)
+    timesetting_values = [setting_list[key] for key in timesetting]
 
     # Scoring Margin -------------------------
     scoring_list ={
@@ -60,7 +70,8 @@ with tab_dashboard:
         '3-Score +': 3
     }
     scoring_keys = scoring_list.keys()
-    st.multiselect("Margin", options=scoring_keys, default=scoring_keys)
+    margin = st.multiselect("Margin", options=scoring_keys, default=scoring_keys)
+    margin_values = [scoring_list[key] for key in margin]
 
     # Leading/Trailing/Tied ------------------
     leading_list = {
@@ -69,8 +80,8 @@ with tab_dashboard:
         'Trailing': 2
     }
     leading_keys = leading_list.keys()
-    st.multiselect("Score:", options=leading_keys, default=leading_keys)
-
+    score = st.multiselect("Score:", options=leading_keys, default=leading_keys)
+    score_values = [leading_list[key] for key in score]
     # Select Yardage -------------------------
     yard_line_range = st.slider(
         "Select a range of yard lines",
@@ -85,14 +96,53 @@ with tab_dashboard:
     start_position = map_yard_line(start_yard)
     end_position = map_yard_line(end_yard)
     st.write(f"{start_position} to {end_position}")
+    # Calculate Yardage to Go
+
+    start_yards_to_go = calculate_yards_to_go(start_yard)
+    end_yards_to_go = calculate_yards_to_go(end_yard)
+    yard_line_range = list(range(end_yards_to_go, start_yards_to_go + 1))
 
     # Down -----------------------------------
     downs = [1, 2, 3, 4]
-    st.multiselect("Down: ", options = downs, default=downs)
+    down = st.multiselect("Down: ", options = downs, default=downs)
 
     # Distance -------------------------------
     distances = list(range(1, 31))
-    st.slider("Distance: ", min_value=1, max_value=30, value =(1, 10), step =1)
+    distance = st.slider("Distance: ", min_value=1, max_value=30, value =(1, 10), step =1)
+    start_distance, end_distance = distance
+    distance_range = list(range(start_distance, end_distance + 1))
+    # Consolidate Parameters
+    parameters = {
+    "selected_team_ids": selected_team_ids,
+    "homeaway": homeaway_list,
+    "timesetting": timesetting_values,
+    "margin": margin_values,
+    "yard_line_range": yard_line_range,
+    "score": score_values,
+    "down": down,
+    "distance_range": distance_range
+}
+
+    data = parameterized_data(parameters)
+
+
+    # DATA VISUALIZATION
+
+    # Calculate KPIs
+    kpis = calc_kpi_playbreakdown(data)
+    labels = list(kpis.keys())
+    values = list(kpis.values())
+    # Create Pie Chart
+    fig = px.pie(values=values, names=labels, title="Play Breakdown")
+
+    # Display Pie Chart in Streamlit
+    st.plotly_chart(fig)
+
+
+
+
+
+
 
     # Disclaimer
 
@@ -100,3 +150,4 @@ with tab_dashboard:
     st.markdown('''
                 *I do not own the data. All data is sourced via NFL API (Rapid API) via ESPN Stats.*
                 ''')
+    
--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import streamlit as st
 import json
 import requests
 #from pandas import json_normalize
@@ -19,3 +20,255 @@ def map_yard_line(yard):
         return f"Opposing {50 - yard}"
     else:  # Midfield
         return "Midfield (50-yard line)"
+    
+def calculate_yards_to_go(yard):
+    if yard < 0:  # Own side (negative yard line)
+        return 50 + abs(yard)  # Distance to the 50-yard line plus the remaining yards
+    elif yard > 0:  # Opponent's side (positive yard line)
+        return 50 - yard  # Distance to the opponent's end zone
+    else:  # Midfield (0-yard line)
+        return 50  # Halfway to the opponent's end zone
+    
+# Load Team Data
+def load_team_data():
+    # Load Team Data
+    teamGameData = pd.read_csv('data/event_team_data.csv', index_col=False)
+    teamGameData = teamGameData.drop_duplicates()
+
+    # Load Team List
+    teams = load_teams()
+
+    # Join Data
+    teamData = pd.merge(teamGameData, teams[['teamId', 'abbreviation']], on='teamId', how='left')
+
+    # Process Data
+
+    # 1. Pivot to flatten data
+    df_home = teamData[teamData['homeAway'] == 'home'].rename(columns={
+        'teamId': 'home_team_id',
+        'final_score': 'home_score_final',
+        'winner': 'home_winner',
+        'total_record':'home_record',
+        'abbreviation':'home_abbrev'
+    })
+
+    df_away = teamData[teamData['homeAway'] == 'away'].rename(columns={
+        'teamId': 'away_team_id',
+        'final_score': 'away_score_final',
+        'winner': 'away_winner',
+        'total_record': 'away_record',
+        'abbreviation':'away_abbrev'
+    })
+
+    # 2. Merge the home and away DataFrames on 'gameId'
+    df_flat = pd.merge(df_home[['gameId', 'home_team_id', 'home_score_final', 'home_winner', 'home_record', 'home_abbrev']],
+                    df_away[['gameId', 'away_team_id', 'away_score_final', 'away_winner', 'away_record', 'away_abbrev']],
+                    on='gameId', how='left')
+
+    # 3. Determine the winning team based on the 'winner' column
+    df_flat['winning_team_id'] = df_flat.apply(lambda row: row['home_team_id'] if row['home_winner'] else row['away_team_id'], axis=1)
+
+    # 4. Drop the intermediate winner columns
+    teamData_flat = df_flat.drop(columns=['home_winner', 'away_winner'])
+
+
+    return teamData_flat
+
+# Load Play By Play Data
+def load_pbp_data():
+    pbp = pd.read_csv('data/event_playbyplay.csv', index_col=False)
+
+    # Clean Play Types
+    excluded_types = [53, 74, 2, 75, 15, 66, 65, 32, 21, 12, 79, 2]
+    pbp = pbp[~pbp['type_id'].isin(excluded_types)].drop_duplicates()
+
+    return pbp
+
+# Process + Clean Data
+def data_processing():
+
+    # Load Data
+    teamData = load_team_data()
+    pbp = load_pbp_data()
+
+    # Join Data
+    pbp_clean = pd.merge(pbp, teamData, how='left', on ='gameId')
+
+    # Adding Home + Away Team Ids
+    pbp_clean['is_home_team'] = (pbp_clean['start_team_id'] == pbp_clean['home_team_id'])
+
+    # Adding Time in Seconds
+    pbp_clean['time_in_seconds'] = pbp_clean.apply(convert_to_seconds, axis=1)
+
+    # Adding Time Bucket
+    pbp_clean['time_bucket'] = pbp_clean.apply(time_buckets, axis=1)
+
+    # Adding Scoring Margin
+    pbp_clean['score_margin'] = pbp_clean.apply(margin_bucket, axis=1)
+
+    # Adding Game Status
+    pbp_clean['score_status'] = pbp_clean.apply(game_status, axis=1)
+
+    # Adding Transformed Yard Line
+    pbp_clean['start_yardLine_transformed'] = pbp_clean.apply(transform_yard_line, axis=1)
+
+    # Adding Yard Line Bucket
+    pbp_clean['yardLine_bucket'] = pbp['start_yardLine'].apply(categorize_yard_line)
+
+    return pbp_clean
+
+# PROCESSING FUNCTIONS -----------------------------------------------------------------------------
+
+# Handling Time + Clock
+def convert_to_seconds(row):
+    minutes, seconds = map(int, row['clock'].split(':'))
+    total_seconds = minutes * 60 + seconds
+    if row['period'] == 1:
+        return total_seconds + 2700
+    elif row['period'] == 2:
+        return total_seconds + 1800 
+    elif row['period'] == 3:
+        return total_seconds + 900  
+    elif row['period'] == 4:
+        return total_seconds  
+
+def time_buckets(row):
+    if row['time_in_seconds'] >= 2700:
+        return 1
+    elif row['time_in_seconds'] >= 1950:
+        return 2
+    elif row['time_in_seconds'] >= 1800:
+        return 5
+    elif row['time_in_seconds'] >= 900:
+        return 3
+    elif row['time_in_seconds'] >= 150:
+        return 4
+    elif row['time_in_seconds'] >=0:
+        return 5
+
+# Handling Score Margin + Lead Status
+def margin_bucket(row):
+    # Calculate the absolute score margin
+    margin = abs(row['homeScore'] - row['awayScore'])
+    # Categorize margin into buckets
+    if margin == 0:
+        return 0
+    elif margin <= 8:
+        return 1
+    elif margin <= 16:
+        return 2
+    else:
+        return 3
+    
+def game_status(row):
+    if row['is_home_team']:  # Home team is being considered
+        if row['homeScore'] > row['awayScore']:
+            return 1
+        elif row['homeScore'] < row['awayScore']:
+            return 2
+        else:
+            return 0
+    else:  # Away team is being considered
+        if row['awayScore'] > row['homeScore']:
+            return 1
+        elif row['awayScore'] < row['homeScore']:
+            return 2
+        else:
+            return 0
+        
+# Handling Field Position
+def transform_yard_line(row):
+    start_yard_line = row['start_yardLine']
+    if start_yard_line > 50:
+        return 100 - start_yard_line
+    else:
+        return start_yard_line
+
+def categorize_yard_line(yard_line):
+    if 100 >= yard_line >= 90:
+        return "Inside Own 10"
+    elif 90 > yard_line >= 50:
+        return "Own Territory"
+    elif 50 > yard_line >= 20:
+        return "Opposing Territory"
+    elif 20 > yard_line >= 6:
+        return "Red Zone"
+    elif 5 >= yard_line >= 0:
+        return "Inside Opponent's 5"
+    else:
+        return "Invalid Yard Line"
+    
+# FILTERING DATA ------------------------------------------------------------------------------------
+def parameterized_data(parameters):
+
+    # Load PBP Data
+    data = data_processing()
+
+    # Parameters ----------------------------------------
+
+    # 1. Team
+    selected_team_ids = parameters["selected_team_ids"]
+    #st.write('Team IDS:', selected_team_ids)
+
+    # 2. Home + Away
+    homeaway = parameters["homeaway"]
+    #st.write('HomeAway:', homeaway)
+
+    # 3. Time Setting
+    timesetting = parameters["timesetting"]
+    #st.write('Time Setting:', timesetting)
+
+    # 4. Margin
+    margin = parameters["margin"]
+    #st.write('Margin:', margin)
+
+    # 5. Yard Line Range
+    yard_line_range = parameters["yard_line_range"]
+    #st.write('Yard Line Range:', yard_line_range)
+
+    # 6. Score
+    score = parameters["score"]
+    #st.write('Score:', score)
+
+    # 7. Down
+    down = parameters["down"]
+    #t.write('Down:', down)
+
+    # 8. Distance
+    distance = parameters["distance_range"]
+    #st.write('Distance Range:', distance)
+
+
+    # Filtered Data
+    subset = data[
+        (data['start_team_id'].isin(selected_team_ids)) &
+        (data['is_home_team'].isin(homeaway)) &
+        (data['start_yardLine'].isin(yard_line_range)) &
+        (data['time_bucket'].isin(timesetting)) &
+        (data['score_margin'].isin(margin)) &
+        (data['score_status'].isin(score)) &
+        (data['down'].isin(down)) &
+        (data['distance'].isin(distance))
+    ]
+
+    return subset
+
+# CALCULATING KPIS ---------------------------------------------------------------------------------
+def calc_kpi_playbreakdown(subset):
+    # Define KPIs
+    totalPlays = len(subset)
+
+    # Pass and Run Plays
+    pass_type_ids = [24, 3, 67, 24]
+    run_type_ids = [68, 5]
+
+    # Calculating the KPIs
+    passPlays = len(subset[subset['type_id'].isin(pass_type_ids)])
+    runPlays = len(subset[subset['type_id'].isin(run_type_ids)])
+
+    kpis = {
+        "Pass Plays": passPlays,
+        "Run Plays": runPlays,
+    }
+
+    return kpis
